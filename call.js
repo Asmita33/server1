ef99638270df479d049f8f4668fb2d3cf6a21151
Asmita Yadav <65132535+Asmita33@users.noreply.github.com>
2021-07-05 18:40:35 +0530

    Update call.js

--- call.js
+++ call.js
@@ -9,11 +9,7 @@ remoteVideo.onplaying = () => { remoteVideo.style.opacity = 1 }
 
 let peer
 function init(userId) {
-    peer = new Peer(userId, {
-        host: 'asmita33.github.io',
-        port: 443,
-        path: '/server1'
-    })
+    peer = new Peer(userId)
     peer.on('open', () => {
     Android.onPeerConnected()
     })
